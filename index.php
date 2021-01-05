056aabfcdc967f63c35fda4b28d7cce2aaf567ef
vu lan <1851060148@e.tlu.edu.vn>
2021-01-06 05:34:18 +0700

    them file bai tap lon ver.1

---
+++ index.php
@@ -0,0 +1,118 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">  </head>
+    <link href="fontawesome-free-5.12.0-web/css/all.css" rel="stylesheet">  
+    <link rel="stylesheet" href="css/root.min.css">
+  </head>
+  <body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-sm-12 col-md-5 col-lg-3 text-white sidebar background-blue">
+                    <div class="mx-auto">
+                        <figure class="profile-image mx-auto">
+                            <img class="img-fluid rounded-circle" src="img/unnamed.png" alt="profile_pic">
+                            <figcaption class="text-center"><small class="d-block">Vu</small>Linh</figcaption>
+                        </figure>
+                        <ul class="list-unstyled main-menu mt-5">
+                        <li><a href="#about-me" class="font-weight-bold text-white"><i class="fa fa-user-tie"></i>About Me</a></li>
+                        <li><a href="experience" class="font-weight-bold text-white"><i class="fa fa-book"></i>Experience</a></li>
+                        <li><a href="portfolio" class="font-weight-bold text-white"><i class="fa fa-toolbox"></i>Portfolio</a></li>
+                        <li><a href="contact" class="font-weight-bold text-white"><i class="fa fa-envelope"></i>Contact</a></li>
+                        </ul>
+                        <ul class="list-unstyled list-inline mt-3 text-center">
+                            <li class= "list-inline-item"><a href="#" class="text-white"><i class="fab fa-facebook-f"></i></a></li>
+                            <li class= "list-inline-item"><a href="#" class="text-white"><i class="fab fa-instagram"></i></a></li>
+                            <li class= "list-inline-item"><a href="#" class="text-white"><i class="fab fa-youtube"></i></a></li>
+                            <li class= "list-inline-item"><a href="#" class="text-white"><i class="fab fa-twitter"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12 col-sm-12 col-md-7 col-lg-9 content about-me active">
+                    <div class="card about-me">
+                        <h3 class="sideline font-weight-bold mb-2">About Me</h3>
+                        <div class="font-weight-bold label title mb-2"><i class="fas fa-code"></i>Web Deverloper - Designer</div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus nesciunt dignissimos possimus voluptatibus ad, cum nemo hic recusandae architecto voluptas consectetur nisi. Quas cupiditate vitae recusandae ex dolore amet ipsum!</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, incidunt. Blanditiis sapiente similique tenetur vitae laudantium modi sed pariatur!</p>
+                        <h3 class="sideline font-weight-bold mb-3">Hobbies and Interests</h3>
+                        <div class="row interests">
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-tree mb-3"></i>
+                                <h6 class="font-weight-bold">Outdoors</h6>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-guitar mb-3"></i>
+                                <h6 class="font-weight-bold">Guitar</h6>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-book mb-3"></i>
+                                <h6 class="font-weight-bold">Books</h6>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-desktop mb-3"></i>
+                                <h6 class="font-weight-bold">Computers</h6>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-gamepad mb-3"></i>
+                                <h6 class="font-weight-bold">Games Dev</h6>
+                            </div>
+                            <div class="col-4 col-md-4 col-lg-2 text-center mt-3">
+                                <i class="text-primary fas fa-plane mb-3"></i>
+                                <h6 class="font-weight-bold">Travel</h6>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mt-5">
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-3">
+                            <card class="card card--stats d-flex flex-row align-items-center">
+                                <i class="text-primary fa fa-award ml-2"></i>
+                                <text-wrapper>
+                                    <h5 class="font-weight-bold ml-3 mb-0">3 Years</h5>
+                                    <p class="ml-3 mb-0">Experiences</p>
+                                </text-wrapper>
+                            </card>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-3">
+                            <card class="card card--stats d-flex flex-row align-items-center">
+                                <i class="text-primary fa fa-users"></i>
+                                <text-wrapper>
+                                    <h5 class="font-weight-bold ml-3 mb-0">30 Clients</h5>
+                                    <p class="ml-3 mb-0">Satisfied</p>
+                                </text-wrapper>
+                            </card>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-3">
+                            <card class="card card--stats d-flex flex-row align-items-center">
+                                <i class="text-primary fa fa-check-circle"></i>
+                                <text-wrapper>
+                                    <h5 class="font-weight-bold ml-3 mb-0">15 Projects</h5>
+                                    <p class="ml-3 mb-0">Delivered</p>
+                                </text-wrapper>
+                            </card>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-6 col-lg-3 mb-3">
+                            <card class="card card--stats d-flex flex-row align-items-center">
+                                <i class="text-primary fa fa-wrench"></i>
+                                <text-wrapper>
+                                    <h5 class="font-weight-bold ml-3 mb-0">10 Avaiable</h5>
+                                    <p class="ml-3 mb-0">For Hire</p>
+                                </text-wrapper>
+                            </card>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>    </body>
+</html>
