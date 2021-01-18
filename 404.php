4434ffa6f2deb7925ba15800972f47db097b4b22
linh16p <1851060148@e.tlu.edu.vn>
2021-01-19 03:08:20 +0700

    change link back and add border

--- 404.php
+++ 404.php
@@ -6,10 +6,12 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="./favicon/favicon (1).ico" rel="icon" type="image/x-icon" />  
+  <link href="./favicon/favicon.ico" rel="icon" type="image/x-icon" />  
+    <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link href="fontawesome-free-5.12.0-web/css/all.css" rel="stylesheet">
   <title>Page not found</title>
-  <link rel="icon" href="./public/favicon.ico"/> <meta name="viewport" content="width=device-width, initial-scale=1"/>
-  <style>
+  <link href="/favicon/favicon.ico" rel="icon" type="image/x-icon" />  <style>
     @font-face {
       src: url("./fonts/cubano-regular-webfont.ttf");
       font-family: "cubano-regular";
@@ -35,18 +37,35 @@
     .page-not-found .go-back a {
       text-decoration: none;
     }
+    .content{
+      margin-left: 30%;
+      margin-right: 30%;
+    }
   </style>
 </head>
 <body>
-  <div class="page-not-found">
-    <div class="content">
+  <div class="page-not-found mt-5">
+    <div class="border border-warning rounded content">
       <h1>404</h1>
+      <hr>
       <h5>Page not found.</h5>
       <p class="qoutes">We couldn't find what you were looking for!</p>
     </div>
     <div class="go-back">
-      <a href="<?php echo constant("URL"); ?>">Go to the front page!</a>
+      <?php 
+      session_start();
+      if(!isset($_SESSION["login"])){
+          header("location: ".constant("URL")."/server/");
+      }else{
+          ?>
+      <a href="<?php echo constant("URL")."/server/dashboard.php"; ?>">Go to the front page!</a>
+      <?php
+      } ?>
     </div>
   </div>
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="js/jquery-3.5.1.min.js"></script>
+ <!-- Bootstrap core JavaScript -->
+	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
