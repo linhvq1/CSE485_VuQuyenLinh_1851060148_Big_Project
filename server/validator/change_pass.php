6b1d49a7900b8389ca9a75d7fe4bca976aebe457
linh16p <1851060148@e.tlu.edu.vn>
2021-01-21 01:30:06 +0700

    add check confirm pass

--- server/validator/change_pass.php
+++ server/validator/change_pass.php
@@ -5,16 +5,21 @@
     if(isset($_POST["changePassword"])){
         $getNewPass = $_REQUEST["newPass"];
         $getConfirmPass = $_REQUEST["confirmPass"];
-        $admin = queryhandler("SELECT email FROM admin LIMIT 1","get");
-        if(password_verify($admin[0]["email"],$_SESSION["hashEmail"])){
-            $hashNewPass = password_hash($getNewPass, PASSWORD_DEFAULT);
-            if(queryhandler("UPDATE admin SET password=? WHERE email=?","set",[$hashNewPass,$admin[0]["email"]])){
-                unset($_SESSION["hashEmail"]);
-                unset($_SESSION["send"]);
-                //               
-                header("location:".constant("URL")."/server/index.php");
-            } else{
-                echo "Error!";
+        if($getNewPass == $getConfirmPass){
+            $admin = queryhandler("SELECT email FROM admin LIMIT 1", "get");
+            if (password_verify($admin[0]["email"], $_SESSION["hashEmail"])) {
+                $hashNewPass = password_hash($getNewPass, PASSWORD_DEFAULT);
+                if (queryhandler("UPDATE admin SET password=? WHERE email=?", "set", [$hashNewPass,$admin[0]["email"]])) {
+                    unset($_SESSION["hashEmail"]);
+                    unset($_SESSION["send"]);
+                    //
+                    header("location:".constant("URL")."/server/index.php");
+                } else {
+                    echo "Error!";
+                }
             }
+        }else{
+            $_SESSION['error_pass_confirm'] = "not match";
+            header("location:".constant("URL")."/server/password_reset.php");
         }
     }
